8f62170b282adae68428d1090b1b286fde437328
Glitch (hello-express) <none>
2019-09-13 16:06:49 +0000

    ✈️👮 Checkpoint ./public/client.js:6303385/1753 ./views/index.html:6303385/1675

--- public/client.js
+++ public/client.js
@@ -7,7 +7,28 @@ const notice = new SimpleGDPR({
 });
 
 
-window.body.onLoad = function() {
-  document.
+const signUp = function() {
+  const username = document.querySelector( '#signup_username' )
+  const password_1 = document.querySelector( '#signup_password_1' )
+  const password_2 = document.querySelector( '#signup_password_2' )
+  
+  if (password_1 !== password_2) {
+    document.querySelector( '#password_match_modal' ).setAttribute('checked', true)
+  } else {
+      fetch( '/add_spacecraft', {
+        method:'POST',
+        body:JSON.stringify({ username:username, password:password_1 }),
+        headers: { 'Content-Type': 'application/json' }
+      })
+      .then( function( response ) {
+        
+        })
+      }
+}
+
+
+window.onload = function() {
+  const signupButton = document.querySelector( '#signup_button' )
+  signupButton.onclick = signUp
 }
 
