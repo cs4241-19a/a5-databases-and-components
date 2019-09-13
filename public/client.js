32e35039ef8ceb17429af22e1e2ccbad0127157e
Glitch (hello-express) <none>
2019-09-13 14:00:59 +0000

    🍳🏌 Checkpoint ./public/client.js:6303385/582 ./views/index.html:6303385/427

--- public/client.js
+++ public/client.js
@@ -1,6 +1,7 @@
 const notice = new SimpleGDPR({
   theme: 'modern',
+  title: 'Cookies',
+  message: 'This site uses cookies. By using this site, you consent to the usage of cookies.',
   icons: false,
-  animation: 'slide',
-  float: 'bottom-left',
+  animation: 'slide'
 });
