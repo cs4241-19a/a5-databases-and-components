e10b0edd470d188247893051fb36322c0bd0049b
Glitch (hello-express) <none>
2019-09-13 18:37:50 +0000

    🙍🌶 Checkpoint ./public/home.js:6303385/824 ./views/home.html:6303385/478 ./server.js:6303385/6

--- public/home.js
+++ public/home.js
@@ -1,8 +1,34 @@
+const displayMessages = function(messages) {
+  const displayElement = document.querySelector('#message_display')
+  
+  displayElement.innerHTML = ""
+  
+  for (let i = 0; i < messages.length; i++) {
+    const message = messages[i]
+    displayElement.innerHTML += 
+      `
+        <div>
+          <h4>${message.username}</h4>
+          <p>${message.message}</p>
+        </div>
+      `
+  }
+}
+
+const loadMessages = function() {
+  fetch('/comments', {
+    method:'GET',
+    credentials: 'include',
+  })
+  .then( res => res.json())
+  .then( res => displayMessages )
+}
+
 const submitMessage = function() {
   fetch( '/add_comment', {
     method:'POST',
-    credentials: 'include'
-    body
+    credentials: 'include',
+    body: JSON.stringify({'message': document.querySelector('#message').value })
   })
   .then( console.log )
   .catch( err => console.error ) 
@@ -10,4 +36,5 @@ const submitMessage = function() {
 
 window.onload = function() {
   document.querySelector('#submit_message').onclick = submitMessage
+  loadMessages()
 }
--- server.js
+++ server.js
@@ -138,6 +138,7 @@ app.post(
 
 app.post('/add_comment', isLoggedIn, function (req, res) {
   const username = req.user.username
+  
   const new_comment = {id: shortid.generate(),
                        message: req.body.message,
                        timestamp: (new Date()).getTime(),
