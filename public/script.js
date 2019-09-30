5af269c26d8621fd083b196c6f1fd8c893c1fa17
eos7l <31972892+eos7l@users.noreply.github.com>
2019-09-30 17:03:31 -0400

    copy from a3

---
+++ public/script.js
@@ -0,0 +1,273 @@
+let curUser;
+
+
+const register = function (e) {
+    e.preventDefault();
+    const username = document.getElementById('inputUsername').value,
+        password = document.getElementById('inputPassword').value;
+    console.log("regUsername:" + username);
+    console.log("regPassword:" + password);
+    fetch('/register', {
+        method: 'GET'
+    }).then(function (response) {
+        return response.json()
+    }).then(function (userList) {
+        addUser(userList, username, password);
+        window.location = ('/');
+    })
+};
+
+function addUser(userList, username, password) {
+    console.log("userList:" + userList);
+    let duplicateAccount = false;
+    for (let i = 0; i < userList.length; i++) {
+        if (userList[i].username === username) {
+            duplicateAccount = true;
+            i = userList.length;
+        }
+    }
+    if (duplicateAccount === false) {
+        const json = {
+                'username': username,
+                'password': password
+            },
+            body = JSON.stringify(json);
+        fetch('/register', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body
+        }).then(function (response) {
+        });
+        document.getElementById('inputUsername').value = "";
+        document.getElementById('inputPassword').value = '';
+    }
+}
+
+const login = function (e) {
+    e.preventDefault();
+    const username = document.getElementById('usernameField').value,
+        password = document.getElementById('passwordField').value;
+    console.log("loginUsername:" + username);
+    console.log("loginPassword:" + password);
+    const user = {
+            'username': username,
+            'password': password
+        },
+        body = JSON.stringify(user);
+    console.log(body);
+    fetch('/login', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body
+    }).then(function (response) {
+        if (response.status === 200) {
+            curUser = username;
+            //document.cookie=curUser;
+            window.localStorage;
+            localStorage.setItem('user',curUser);
+            window.location = 'main';
+            displayDB();
+        } else {
+            console.log(response);
+            alert("Authentication failed!")
+        }
+        fetch('/register', {
+            method: 'GET'
+        }).then(function (response) {
+            return response.json()
+        }).then(function (users) {
+            console.log(users)
+        })
+    })
+}
+
+
+const hideForm = function () {
+    console.log("I got here");
+    //e.preventDefault();
+    document.getElementById('formContainer').style.display = "none";
+    document.getElementById('dropDownMenu').style.display = "none";
+    document.getElementById('retailerSelection').style.display = "none";
+    document.getElementById('list-options-container').style.display = "none";
+    document.getElementById('submitBtn').style.display = "none";
+};
+
+const displayDB = function () {
+    hideForm();
+    fetch('/newData', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({ user:getCurUsername })
+    }).then(function (response) {
+        return response.json()
+    }).then(function (dataRow) {
+        fillTableInfo(dataRow, -999)
+    })
+}
+
+const submit = function (e) {
+    e.preventDefault();
+    window.getCurUsername = localStorage.getItem('user');
+    let dropdown = document.getElementById("categoryOptions");
+    const itemName = document.querySelector('#itemName').value,
+        category = dropdown.options[dropdown.selectedIndex].value,
+        list = document.querySelector('input[name="listName"]:checked').value,
+        retailer = document.querySelector('input[name="retailer"]:checked').value;
+         URL = document.querySelector('#url').value;
+    let specifiedListName;
+    switch (list) {
+        case 'need':
+            specifiedListName = 'need';
+            break;
+        case 'want':
+            specifiedListName = 'want';
+            break;
+        default:
+            if (document.getElementById('otherListName').checked) {
+                specifiedListName = document.getElementById('inputListName').value
+            }
+    }
+    const json = {
+            'user': getCurUsername,
+            'itemName': itemName,
+            'category': category,
+            'list': specifiedListName,
+            'oneRetailerOnly': retailer,
+            'URL': URL,
+        },
+        body = JSON.stringify(json);
+    //console.log("before fetch");
+    fetch('/submit', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body
+    }).then(function (response) {
+    })
+    // if(curUser===username){
+    displayDB();
+    //hideForm();
+    //}
+    return false
+};
+
+function changeRadioButton() {
+    var other = document.getElementById("otherListName");
+    other.value = document.getElementById("inputListName").value;
+    other.checked = true;
+    console.log(other.value)
+}
+
+function clearChoice() {
+    document.getElementById("otherListName").value = "";
+    document.getElementById("inputListName").value = "";
+}
+
+const updateRow = function (row) {
+    document.getElementById("itemTable").style.width="auto";
+    //document.querySelector("#itemTable").style.width=auto;
+    let updateItemName = document.getElementById('itemNameInput' + row).value;
+    let updateCategory = document.getElementById('categoryInput' + row).value;
+    let updateList = document.getElementById('listInput' + row).value;
+    let updateRetailer = document.getElementById('retailerInput' + row).value;
+    let updateLink = document.getElementById('linkInput' + row).value;
+    const json = {
+        'itemName': updateItemName,
+        'category': updateCategory,
+        'list': updateList,
+        'oneRetailerOnly': updateRetailer,
+        'URL': updateLink,
+    }
+    json.index = row;
+    const body = JSON.stringify(json);
+    fetch('/update', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body
+    }).then(function (response) {
+        displayDB();
+    })
+};
+
+const editRow = function (i) {
+    console.log("I am in the editRow function");
+    fetch('/newData', {
+        //method: 'GET'
+        method:'POST',
+        headers: {'Content-Type': 'application/json'},
+        body:JSON.stringify({rowNum:i,user:getCurUsername})
+    }).then(function (response) {
+        return response.json()
+    }).then(function (itemData) {
+        fillTableInfo(itemData, i)
+    })
+}
+
+const deleteRow = function (row) {
+    const deletedData = {deletedData: row};
+    console.log(row);
+    const body = JSON.stringify(deletedData);
+    fetch('/delete', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body
+    });
+    displayDB();
+};
+
+const fillTableInfo = function (shoppingData, editRowNum) {
+    let wishListTable = document.querySelector('#itemTable');
+    wishListTable.innerHTML =
+        '<tr>\n' +
+        '<th >Item Name</th>\n' +
+        '<th >Category</th>\n' +
+        '<th >List</th>\n' +
+        '<th >Specified Retailer Only?</th>\n' +
+        '<th >URL</th>\n' +
+        '<th >Edit</th>\n' +
+        '<th >Delete</th>\n' +
+        '</tr>';
+    console.log("I am in the fill table function")
+    for (let i = 0; i < shoppingData.length; i++) {
+        console.log("I am in the for loop")
+        const userItemChoice = shoppingData[i];
+        if (userItemChoice.user === getCurUsername) {
+            let newLine = '<tr>\n';
+            if (i === editRowNum) {
+                newLine += ('<td>' + '<input id="itemNameInput' + i + '" type="text" value="' + userItemChoice.updatedItem + '"> </div></td>\n');
+                newLine += ('<td>' + '<input id="categoryInput' + i + '" type="text" value="' + userItemChoice.updatedCategory + '"></div></td>\n');
+                newLine += ('<td>' + '<input id="listInput' + i + '" type="text" value="' + userItemChoice.updatedList + '"> </div></td>\n');
+                newLine += ('<td>' + '<input id="retailerInput' + i + '" type="text" value="' + userItemChoice.updatedRetailer + '"></div></td>\n');
+                newLine += ('<td>' + '<input id="linkInput' + i + '" type="text" value="' + userItemChoice.updatedLink + '"> </div></td>\n');
+                newLine += ('<td>' + '<button  class="btn btn-sm" id="update' + i + '" onclick="updateRow(' + i + ')"> Update </button></div></td>\n');
+                newLine += ('<td>' + '<button  class="btn btn-sm"  id="delete' + i + '" onclick="deleteRow(' + i + ')"> X </button></div></td>\n');
+                newLine += '</tr>';
+            } else {
+                newLine += ('<td>' + userItemChoice.itemName + '</div></td>\n');
+                newLine += ('<td>' + userItemChoice.category + '</td>\n');
+                newLine += ('<td>' + userItemChoice.list + '</td>\n');
+                newLine += ('<td>' + userItemChoice.oneRetailerOnly + '</td>\n');
+                newLine += ('<td>' + userItemChoice.URL + '</div></td>\n');
+                newLine += ('<td>' + '<button class="btn btn-sm" id="' + i + '" onclick="editRow(' + i + ')"> Edit </button></td>\n');
+                newLine += ('<td>' + '<button  class="btn btn-sm" id="' + i + '" onclick="deleteRow(' + i + ')"> X </button></td>\n');
+                newLine += '</div>' + '</tr>';
+            }
+            wishListTable.innerHTML += newLine
+        }
+    }
+};
+
+window.onload = function () {
+    fetch('/test', {
+        method: 'POST',
+        credentials: 'include'
+    }).then(console.log)
+        .catch(err => console.error);
+    const registerButton = document.getElementById('registerBtn');
+    const loginButton = document.getElementById('loginBtn');
+    if (registerButton !== null) {
+        registerButton.onclick = register;
+    }
+    if (loginButton !== null) {
+        loginButton.onclick = login;
+    }
+};
---
+++ server.js
@@ -0,0 +1,237 @@
+const express = require('express'),
+    app = express(),
+    low = require('lowdb'),
+    FileSync = require('lowdb/adapters/FileSync'),
+    adapter = new FileSync('data/db2.json'),
+    db = low(adapter),
+    passport = require('passport'),
+    Local = require('passport-local').Strategy,
+    session = require('express-session'),
+    bodyparser = require('body-parser'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    path = require('path'),
+    helmet = require('helmet'),
+    router = express.Router(),
+    compression = require('compression'),
+    dir='public/',
+    port = 8000;
+
+
+const appdata = [
+        {   "user":"admin",
+            "itemName": "Son & Park Beauty Water",
+            "category": "Health & Beauty",
+            "list": "need",
+            "oneRetailerOnly": "Yes",
+            "URL": "https://seph.me/2kxrFgd"
+        },
+        {   "user":"admin",
+            "itemName": "Givenchy Small GV3 Leather Shoulder Bag",
+            "category": "Clothes & Handbags",
+            "list": "want",
+            "oneRetailerOnly": "No",
+            "URL": "http://bit.ly/2md33JW"
+        },
+        {   "user":"admin",
+            "itemName": "Lenovo Legion Y740",
+            "category": "Electronics & Computers",
+            "list": "want",
+            "oneRetailerOnly": "No",
+            "URL": "https://lnv.gy/2lRz8a3"
+        },
+        {   "user":"admin",
+            "itemName": "Alienware Aurora R8 Desktop",
+            "category": "Electronics & Computers",
+            "list": "need",
+            "oneRetailerOnly": "Yes",
+            "URL": "https://dell.to/2mgSaHc"
+        }
+    ]
+const users = [
+    {username: 'swain', password: 'cain'},
+    {username: 'eos7l', password: 'swdw'},
+    {username:'admin', password: 'CS4241'}
+]
+
+db.defaults({appdata: appdata, users: users}).write();
+
+// automatically deliver all files in the public folder
+// with the correct headers / MIME type.
+app.use(express.static(dir));
+//app.use(cookieParser());
+app.use(helmet());
+app.use(compression());
+app.use(bodyparser.json());
+app.use(passport.initialize());
+
+//passport.use( new Local( myLocalStrategy));
+app.get('/', function (req, res) {
+    // Cookies that have not been signed
+    console.log('Cookies: ', req.cookies);
+    // Cookies that have been signed
+    console.log('Signed Cookies: ', req.signedCookies);
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+// app.get('/loggeduser', function (req, res) {
+//     if (req.user === undefined) {
+//         res.json({});
+//     } else {
+//         res.json({
+//             username: req.user
+//         });
+//     }
+// });
+
+app.get('/main', function (req, res) {
+    // Cookies that have not been signed
+    console.log('Cookies: ', req.cookies);
+    // Cookies that have been signed
+    console.log('Signed Cookies: ', req.signedCookies);
+    res.sendFile( path.join(__dirname, '/public/main.html'));
+});
+
+// all authentication requests in passwords assume that your client
+// is submitting a field named "username" and field named "password".
+// these are both passed as arugments to the authentication strategy.
+const myLocalStrategy = function( username, password, done ) {
+  // find the first item in our users array where the username
+  // matches what was sent by the client. nicer to read/write than a for loop!
+  const user = db.get('users').value().find( __user => __user.username === username );
+  // if user is undefined, then there was no match for the submitted username
+  if( user === undefined ) {
+    /* arguments to done():
+     - an error object (usually returned from database requests )
+     - authentication status
+     - a message / other data to send to client
+*/
+    return done( null, false, { message:'user not found' })
+  }else if( user.password === password ) {
+    // we found the user and the password matches!
+    // go ahead and send the userdata... this will appear as request.user
+    // in all express middleware functions.
+    return done( null, { username, password })
+  }else{
+    // we found the user but the password didn't match...
+    return done( null, false, { message: 'incorrect password' })
+  }
+};
+
+passport.use( 'local-login', new Local( myLocalStrategy ) );
+passport.initialize();
+
+
+app.post('/login',
+    passport.authenticate( 'local-login',{
+       // successRedirect: '/main',
+         //failureRedirect: "/"
+    } ),
+    function( req, res ) {
+        console.log( 'user:', req.user );
+        res.json({ status: true });
+    }
+);
+
+passport.serializeUser( ( user, done ) => done( null, user.username ) );
+// "name" below refers to whatever piece of info is serialized in seralize User,
+// in this example we're using the username
+passport.deserializeUser( ( username, done ) => {
+  const user = users.find( u => u.username === username );
+  console.log( 'deserializing:', name );
+
+  if( user !== undefined ) {
+    done( null, user )
+  }else{
+    done( null, false, { message:'user not found; session not restored' })
+  }
+});
+app.use( session({ secret:'topSecret', resave:false, saveUninitialized:false }) );
+app.use( passport.initialize() );
+app.use( passport.session() );
+
+app.post('/test', function( req, res ) {
+  console.log( 'authenticate with cookie?', req.user );
+  res.json({ status:'success' })
+});
+
+app.post('/newData', (req, res) => {
+    console.log(req.body);
+    let data = db.get('appdata').filter({ user:req.body.user }).value();
+    //let data = db.get('appdata').value();
+    console.log(data);
+    res.send(data);
+});
+/*
+app.get('/newData', (req, res) => {
+    if(req.user===undefined){
+        res.redirect(401,'/login')
+    }
+    else{
+        let curUser=req.user.username;
+        res.set('Content-Type', 'application/json');
+        let data = db.find({ 'user': curUser }).get('appdata').value();
+        res.send(data);
+    }
+});
+*/
+app.get('/register', (req, res) => {
+    let data = db.get('users').value();
+    res.send(data)
+});
+
+app.post('/register', (req, res) => {
+    let data = req.body;
+    db.get('users').push(data).write();
+    res.status(200).send("Added user to database");
+});
+
+app.post('/submit', function (req, res) {
+    let data = req.body;
+    const pushData={
+        'user':data.user,
+        'itemName': data.itemName,
+        'category': data.category,
+        'list': data.list,
+        'oneRetailerOnly':data.oneRetailerOnly,
+        'URL': data.URL,
+    };
+    db.get('appdata').push(pushData).write();
+    res.status(200).send("pushed!");
+});
+
+/*
+app.post('/submit', function (req, res) {
+    if(req.user===undefined){
+        res.redirect(401,'/')
+    }
+    else{
+        let curUser=req.user.username;
+        let data=req.body;
+        db.find({ 'user': curUser }).get('appdata').push(data).write();
+        res.status(200).send("pushed!");
+    }
+});*/
+
+app.post('/update', function (req, res) {
+    const index = req.body.index,
+        indexVal = db.get('appdata[' + index + ']').value();
+    db.get('appdata').find(indexVal).assign({
+        itemName: req.body.itemName,
+        category: req.body.category,
+        list: req.body.list,
+        oneRetailerOnly: req.body.oneRetailerOnly,
+        URL: req.body.URL
+    }).write();
+    res.status(200).send("updated!")
+})
+
+app.post('/delete', function (req, res) {
+    const index = req.body.deletedData,
+        indexVal = db.get('appdata[' + index + ']').value();
+    db.get('appdata').remove(indexVal).write();
+    res.status(200).send("deleted!")
+})
+
+
+app.listen(process.env.PORT || port)
