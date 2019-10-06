a3e845ed00d384cff472c9824b50f0a35b699d95
Elie <eliemhess@gmail.com>
2019-10-06 19:22:03 -0400

    Login works

--- server.js
+++ server.js
@@ -6,25 +6,23 @@ If you're seeing this, do me a favor and watch this video before proceeding with
 https://www.youtube.com/watch?v=CQ85sUNBK7w
 */
 const low = require('lowdb'),
-  express = require('express'),
-  compression = require('compression'),
-  session = require('express-session'),
-  passport = require('passport'),
-  flash = require("connect-flash"),
-  Local = require('passport-local').Strategy,
-  FileSync = require('lowdb/adapters/FileSync'),
-  http = require("http"),
-  fs = require("fs"),
-  mime = require("mime"),
-  helmet = require("helmet"),
-  bodyParser = require('body-parser'),
-  responseTime = require('response-time'),
-  morgan = require('morgan'),
-  adapter = new FileSync('db.json'),
-  db = low(adapter),
-  app = express(),
-  dir = "public/",
-  port = 3000;
+    express = require('express'),
+    compression = require('compression'),
+    session = require('express-session'),
+    passport = require('passport'),
+    Local = require('passport-local').Strategy,
+    //FileSync = require('lowdb/adapters/FileSync'),
+    mime = require("mime"),
+    helmet = require("helmet"),
+    bodyParser = require('body-parser'),
+    responseTime = require('response-time'),
+    morgan = require('morgan'),
+    mongodb = require('mongodb'),
+    //adapter = new FileSync('db.json'),
+    //db = low(adapter),
+    app = express(),
+    dir = "public/",
+    port = 3000;
 
 app.use(express.static('public'));
 app.use(helmet());
@@ -32,34 +30,51 @@ app.use(compression());
 app.use(bodyParser.json());
 app.use(responseTime());
 app.use(morgan('tiny'));
-app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).send('Bad thing happened');
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Bad thing happened');
 });
 
-passport.use(new Local(function(username, password, done) {
-  const user = db.get("members").find({
-    "username": username
-  }).value();
+const MongoClient = mongodb.MongoClient;
+const uri = "mongodb+srv://admin:admin@a5-eliehess-98hnm.mongodb.net/admin?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+let users = null
 
-  if (user === undefined) {
-    console.log("user not found")
-    return done(null, false, {
-      message: "user not found"
-    });
-  } else if (user.password === password) {
-    console.log("user found")
-    return done(null, {
-      username,
-      password
+client.connect()
+    .then(() => {
+        return client.db('MTG').createCollection('users')
     })
-  } else {
-    console.log("incorrect password")
-    return done(null, false, {
-      message: "incorrect password"
-    });
-  }
+    .then(__collection => {
+        // store reference to collection
+        users = __collection
+        // blank query returns all documents
+        return users.find({}).toArray()
+    })
+    .then(console.log)
 
+passport.use(new Local(function (username, password, done) {
+    users.findOne({username: username}).then((response) => {
+        console.log("r: " + response)
+        console.log(response.password)
+        console.log(response.username)
+        if (!response) {
+            console.log("user not found")
+            return done(null, false, {
+                message: "user not found"
+            });
+        } else if (response.password === password) {
+            console.log("user found")
+            return done(null, {
+                username,
+                password
+            })
+        } else {
+            console.log("incorrect password")
+            return done(null, false, {
+                message: "incorrect password"
+            });
+        }
+    })
 }));
 
 passport.initialize();
@@ -67,96 +82,130 @@ passport.initialize();
 passport.serializeUser((user, done) => done(null, user.username))
 
 passport.deserializeUser((username, done) => {
-  const user = db.get("members").find(u => u.username === username)
-
-  if (user !== undefined) {
-    done(null, user)
-  } else {
-    done(null, false, {
-      message: 'user not found; session not restored'
+    users.findOne({
+        username: username
+    }).then((user) => {
+        if (user !== undefined) {
+            done(null, user)
+        } else {
+            done(null, false, {
+                message: 'user not found; session not restored'
+            })
+        }
     })
-  }
 });
 
 app.use(session({
-  secret: 'fifteen potatoes',
-  resave: false,
-  saveUninitialized: false
+    secret: 'fifteen potatoes',
+    resave: false,
+    saveUninitialized: false
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.post('/login', passport.authenticate('local'), function(req, res) {
-  console.log('user: ', req.user)
-  res.json({
-    status: true
-  })
+app.post('/login', passport.authenticate('local'), function (req, res) {
+    console.log('user: ', req.user)
+    res.json({
+        status: true
+    })
 });
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/getdata', function(req, res) {
-  const type = mime.getType(db.get('members').value());
-  res.writeHeader(200, {
-    "Content-Type": type
-  });
-  res.write(JSON.stringify({
-    data: db.get('members').value()
-  }));
-  res.flush();
-  res.end();
-});
+/*app.get('/getdata', function (req, res) {
+    const type = mime.getType(db.get('members').value());
+    res.writeHeader(200, {
+        "Content-Type": type
+    });
+    res.write(JSON.stringify({
+        data: db.get('members').value()
+    }));
+    res.flush();
+    res.end();
+});*/
 
-app.post('/update', function(req, res) {
-  const updatedEntry = {
-    "firstname": req.body.firstname,
-    "lastname": req.body.lastname,
-    "username": req.body.username,
-    "password": req.body.password
-  };
-
-  db.get('members').remove({
-    username: updatedEntry.username
-  }).write();
-
-  db.get('members').push(updatedEntry).write();
-
-  res.writeHead(200, "OK", {
-    "Content-Type": "text/plain"
-  });
-  res.end();
-});
-
-app.post('/submit', function(req, res) {
-  const newMember = {
-    "firstname": req.body.firstname,
-    "lastname": req.body.lastname,
-    "username": req.body.username,
-    "password": req.body.password
-  };
-
-  db.get('members').push(newMember).write();
-
-  res.writeHead(200, "OK", {
-    "Content-Type": "text/plain"
-  });
-  res.end();
+app.get('/getdata', (req, res) => {
+    if (users !== null) {
+        users.find({}).toArray().then(result => {
+            /*res.writeHeader(200, {
+                "Content-Type": type
+            });*/
+            res.write(JSON.stringify({
+                data: result
+            }));
+            console.log("get: " + result)
+            /*res.flush();
+            res.end();*/
+        })
+    }
 })
 
-app.post('/delete', function(req, res) {
-  db.get('members').remove({
-    username: req.body.username
-  }).write();
-  res.writeHead(200, "OK", {
-    "Content-Type": "text/plain"
-  });
-  res.end();
+app.post('/update', function (req, res) {
+    const updatedEntry = {
+        "firstname": req.body.firstname,
+        "lastname": req.body.lastname,
+        "username": req.body.username,
+        "password": req.body.password,
+        _id: makeid(req.body.username)
+    };
+
+    /*db.get('members').remove({
+        username: updatedEntry.username
+    }).write();
+
+    db.get('members').push(updatedEntry).write();*/
+
+    users.deleteOne({_id: mongodb.ObjectID(updatedEntry._id)})
+    users.insertOne(updatedEntry).then(() => {
+        res.writeHead(200, "OK", {
+            "Content-Type": "text/plain"
+        });
+        res.end();
+    })
 });
 
-app.listen(process.env.PORT || port, function() {
-  console.log("Server running on port " + port);
-  console.log("Press Ctrl + C to stop");
+app.post('/submit', function (req, res) {
+    const newMember = {
+        "firstname": req.body.firstname,
+        "lastname": req.body.lastname,
+        "username": req.body.username,
+        "password": req.body.password,
+        _id: makeid(req.body.username)
+    };
+
+    //db.get('members').push(newMember).write();
+    users.insertOne(newMember).then(() => {
+        res.writeHead(200, "OK", {
+            "Content-Type": "text/plain"
+        });
+        res.end();
+    })
+})
+
+app.post('/delete', function (req, res) {
+    /*db.get('members').remove({
+        username: req.body.username
+    }).write();
+    res.writeHead(200, "OK", {
+        "Content-Type": "text/plain"
+    });
+    res.end();*/
+    users.deleteOne({_id: mongodb.ObjectID(req.body._id)})
+        .then(result => res.json(result))
 });
+
+app.listen(process.env.PORT || port, function () {
+    console.log("Server running on port " + port);
+    console.log("Press Ctrl + C to stop");
+});
+
+const makeid = function (str) {
+    let fin = 0
+    str.split('').forEach(function (c) {
+        fin += c.charCodeAt(0)
+    });
+    return fin
+}
