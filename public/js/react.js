bab8bf3573c8e28e92f3c8431915f853c0c5f7cd
Glitch (a5-nbloniarz) <none>
2019-10-06 19:42:20 +0000

    👐🌊 Checkpoint ./public/js/react.js:6266250/1176 ./public/admin.html:6266250/13907 ./public/css/style.css:6266250/213

--- public/js/react.js
+++ public/js/react.js
@@ -1,6 +1,30 @@
-import ReactDOM from 'react-dom'
+'use strict';
 
-function sessionTimer(){
-  const element = (<div><p>Current Time: {new Date().toLocalTimeString()}</p></div>)
-  ReactDOM.render(element. document.getElementById('root'));
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { timer: 0 };
+    this.tick()
   }
+
+  render() {
+    if (this.state.timer > 0) {
+      return this.state.timer;
+    }
+
+    return e(
+      'p',
+      'Session Timer: ',
+      this.state.timer},
+    );
+  }
+  
+  tick(){
+     setInterval( ()=> this.setState({ anumber:this.state.anumber + 1 }), 1000 )
+  }
+}
+
+const domContainer = document.getElementById('reactContainer');
+ReactDOM.render(e(LikeButton), domContainer);
