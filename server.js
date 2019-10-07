26d21a3fa395deffd7792d579cc612fdad39aca3
Glitch (a5-alejandra-garza) <none>
2019-10-07 14:35:49 +0000

    🌞💶 Checkpoint ./views/home.html:6348319/32 ./server.js:6348319/51

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require( 'express' ),
       path    = require( 'path'    ),
       app = express()
 
-app.use( express.static('public') )
+app.use( express.static('/views/home') )
 app.use( express.json() )
 
 //view engine?
