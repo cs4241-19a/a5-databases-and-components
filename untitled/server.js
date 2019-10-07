ca3e5ba73c1e89f5d605d831fc36d1c780f285a3
kandonie <kandonie@wpi.edu>
2019-10-07 11:51:14 -0400

    added mongodb

--- untitled/server.js
+++ untitled/server.js
@@ -1,22 +1,22 @@
-// server.js
-// where your node app starts
+const express = require('express')
+const path = require('path')
+const cool = require('cool-ascii-faces')
+const PORT = process.env.PORT || 5000
+const mongodb = require('mongodb');
 
-// init project
-const express = require("express");
-const app = express();
+express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .get('/cool', (req, res) => res.send(cool()))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-// we've started you off with Express,
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function(request, response) {
-    response.sendFile(__dirname + "/views/views/index.html");
-});
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-    console.log("Your app is listening on port " + listener.address().port);
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://kandonie:password123456@cluster0-7w27d.mongodb.net/admin?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
 });
