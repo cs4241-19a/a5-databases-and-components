2df9b02a6ca74e1127244fd73de05ab85c332274
kandonie <kandonie@wpi.edu>
2019-10-07 10:57:15 -0400

    added mongodb

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const cool = require('cool-ascii-faces')
 const PORT = process.env.PORT || 5000
+const mongodb = require('mongodb');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -10,3 +11,12 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://kandonie:password123456@cluster0-7w27d.mongodb.net/admin?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
