fc9907fa12c5b1ff8487ad87e01c697a3cb69147
kandonie <kandonie@wpi.edu>
2019-10-10 21:23:52 -0400

    did some things

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+var bodyParser = require('body-parser');
 const path = require('path')
 const cool = require('cool-ascii-faces')
 const PORT = process.env.PORT || 5000
 const mongodb = require('mongodb');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -12,11 +15,15 @@ express()
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://kandonie:password123456@cluster0-7w27d.mongodb.net/admin?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
+//const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://kandonie:password123456@cluster0-7w27d.mongodb.net/admin?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//  const collection = client.db("test").collection("devices");
   // perform actions on the collection object
-  client.close();
+//  client.close();
+//});
+
+app.get('/index', function(request, response) {
+  response.sendFile(__dirname + '/views/pages/index.html');
 });
